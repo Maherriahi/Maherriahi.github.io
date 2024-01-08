8ebdaa5114464bcfe9ea532072ca55821c558f93
Maherriahi <riahi8626@gmail.com>
2024-01-08 23:26:57 +0100

    first commit

--- javascript.js
+++ javascript.js
@@ -1,42 +1,42 @@
-let videoProjet = document.querySelector('.video-projet')
-let dashboard = document.querySelector('.dashboard')
-let boutique = document.querySelector('.boutique')
-let library = document.querySelector('.library')
-let sumurai = document.querySelector('.sumurai')
-let close = document.querySelector('.close')
-let source = document.createElement('source')
-let video = document.querySelector('.video-mp4')
-let card_service = document.querySelector('.card-service')
+let videoProjet = document.querySelector('.video-projet');
+let dashboard = document.querySelector('.dashboard');
+let boutique = document.querySelector('.boutique');
+let library = document.querySelector('.library');
+let sumurai = document.querySelector('.sumurai');
+let close = document.querySelector('.close');
+let source = document.createElement('source');
+let video = document.querySelector('.video-mp4');
+let card_service = document.querySelector('.card-service');
 
 
 close.addEventListener('click', function () {
-    console.log('ok')
-    videoProjet.style.display = 'none'
-    window.location.reload()
+    console.log('ok');
+    videoProjet.style.display = 'none' ;
+    window.location.reload();
 
 })
 
 dashboard.addEventListener('click', function () {
-    srcVideo('video/dashboard.mp4','video/Mp4')
-    videoProjet.style.display = 'block'
+    srcVideo('video/dashboard.mp4','video/Mp4');
+    videoProjet.style.display = 'block';
 
 })
 
 boutique.addEventListener('click', function () {
-    srcVideo('video/boutique.mp4','video/mp4')
-    videoProjet.style.display = 'block'
+    srcVideo('video/boutique.mp4','video/mp4');
+    videoProjet.style.display = 'block';
 
 })
 
 library.addEventListener('click', function () {
-    srcVideo('video/library.mp4','video/mp4')
-    videoProjet.style.display = 'block'
+    srcVideo('video/library.mp4','video/mp4');
+    videoProjet.style.display = 'block' ;
 
 })
 
 sumurai.addEventListener('click', function () {
-    srcVideo('video/sumurai.mp4','video/mp4')
-    videoProjet.style.display = 'block'
+    srcVideo('video/sumurai.mp4','video/mp4') ;
+    videoProjet.style.display = 'block' ;
 
 })
 
@@ -44,7 +44,7 @@ function srcVideo(src,type){
 
     source.setAttribute('src', src);
     source.setAttribute('type', type);
-    video.appendChild(source)
+    video.appendChild(source);
 }
 
 
